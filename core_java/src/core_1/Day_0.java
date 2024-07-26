eb8fe3d29943688bbf71d17c06198db277bef80b
durkeshveer <162946489+durkeshveer@users.noreply.github.com>
2024-07-26 15:59:11 +0530

    day 0 and day 1 java project

---
+++ core_java/src/core_1/Day_0.java
@@ -0,0 +1,12 @@
+package core_1;
+
+public class Day_0 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println("fepooqpp");
+			System.out.println("haaaiii bhaa");
+		}
+
+	}
+
---
+++ core_java/src/core_1/Day_1_Basics.java
@@ -0,0 +1,16 @@
+package core_1;
+import java.util.*;
+public class Day_1_Basics {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		sc.nextLine();
+		String name =sc.nextLine();
+		int c=a+b;
+		System.out.println("Sum is "+c);
+		System.out.println("Name is : "+name);
+	}
+
+}
